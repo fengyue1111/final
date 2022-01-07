a286c8e850d92e494cd8ac714bba08dcd4138a7d
豐岳 李 <108111136@mail.aeust.edu.tw>
2022-01-07 12:36:21 +0800

    期末

--- final/final.aspx.designer.cs
+++ final/final.aspx.designer.cs
@@ -78,6 +78,15 @@ namespace final
         protected global::System.Web.UI.WebControls.Button Button1;
 
         /// <summary>
+        /// RequiredFieldValidator1 控制項。
+        /// </summary>
+        /// <remarks>
+        /// 自動產生的欄位。
+        /// 若要修改，請將欄位宣告從設計工具檔案移到程式碼後置檔案。
+        /// </remarks>
+        protected global::System.Web.UI.WebControls.RequiredFieldValidator RequiredFieldValidator1;
+
+        /// <summary>
         /// Tip 控制項。
         /// </summary>
         /// <remarks>
